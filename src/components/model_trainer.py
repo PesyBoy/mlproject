06647fcdd06486823c8641472d25ad450cd80860
Mahamadou Dagnoko <mddevp@gmail.com>
2024-09-08 15:07:28 -0500

    Hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -21,7 +21,7 @@ from src.utils import save_object, evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("artifacs", "model.pkl")
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
     
 class ModelTrainer:
     def __init__(self):
@@ -48,8 +48,57 @@ class ModelTrainer:
                 "AdaBoost Classifier": AdaBoostRegressor(),
             }
             
-            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train,
-                                             X_test = X_test, y_test = y_test, models = models)
+            params = {
+                "Decision Tree":{
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error','poisson'],
+                    # 'splitter': ['best', 'random'],
+                    # 'max_features': ['sqrt', 'log2'],
+                },
+                
+                "Random Forest":{
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error','poisson'],
+                    'max_features': ['sqrt', 'log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[0.1,0.01,0.05, 0.001],
+                    'subsample':[0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto', 'sqrt', 'log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                
+                "Linear Regression":{},
+                "K-Neighbors Classifier":{
+                    'n_neighbors':[3,5,7,9],
+                    # 'weights':['uniform', 'distance'],
+                    # 'algorithm':['ball_tree', 'kd_tree', 'brute']
+                },
+                
+                "XGBClassifier":{
+                    'learning_rate':[0.1, 0.01, 0.05, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                
+                "CatBoosting classifier":{
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.1, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                
+                 "AdaBoost Classifier":{
+                    'learning_rate':[0.1, 0.01, 0.05, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                    'loss':['linear', 'square', 'exponential']
+                },
+                
+            }
+            
+            
+            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train,X_test = X_test,
+                                              y_test = y_test, models = models, params = params)
             
             best_model_score = max(sorted(model_report.values()))
             
